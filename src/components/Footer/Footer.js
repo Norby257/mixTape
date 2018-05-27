bd9843da05086291cab70732a945c5e3402c8a27
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-05-27 18:55:11 -0500

    initial static components and front end

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => (
+    <footer className="footer">
+    <div className="bottom">
+    MixTape | Copyright 2018 Noreen M 
+    </div>
+</footer>
+);
+
+
+export default Footer;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React, { Component} from 'react';
+
+const Navbar = () => (
+    <nav className = "navbar-navbar-inverse">
+    Navbar here 
+    </nav>
+)
+
+export default Navbar;
