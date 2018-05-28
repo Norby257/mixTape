c8d479398f49af47b2a10f40928ac855918ac76c
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-05-28 18:32:30 -0500

    fixed security issue: added rel= noopener noferrer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,9 +5,8 @@ const Footer = () => (
     <h5> Find Us On </h5>
     <div className="w3-xlarge w3-padding-16">
     <i class="fa fa-twitter w3-hover-opacity"></i>
-    <a href=" linkedin.com/in/nmaddenling/"> <i class="fa fa-linkedin w3-hover-opacity"></i></a>
-    <a href="https://github.com/Norby257" target="_blank"> <i class="fa fa-github-square w3-hover-opacity"></i></a>
-
+    <a href=" linkedin.com/in/nmaddenling/" target="_blank" rel="noopener noreferrer"> <i class="fa fa-linkedin w3-hover-opacity"></i></a>
+    <a href="https://github.com/Norby257" target="_blank" rel="noopener noreferrer"> <i class="fa fa-github-square w3-hover-opacity" rel="noopener noreferrer">></i></a>
     </div>
     <div className="bottom">
     MixTape | Copyright 2018 Noreen M 
