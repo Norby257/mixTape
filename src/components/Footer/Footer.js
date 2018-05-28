44b2ea6837f0602d77503915c80fedafcf4dd26d
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-05-28 18:18:24 -0500

    footer complete and font awesome link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,14 @@
 import React from 'react';
-
+import './Footer.css'
 const Footer = () => (
     <footer className="footer">
+    <h5> Find Us On </h5>
+    <div className="w3-xlarge w3-padding-16">
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <a href=" linkedin.com/in/nmaddenling/"> <i class="fa fa-linkedin w3-hover-opacity"></i></a>
+    <a href="https://github.com/Norby257" target="_blank"> <i class="fa fa-github-square w3-hover-opacity"></i></a>
+
+    </div>
     <div className="bottom">
     MixTape | Copyright 2018 Noreen M 
     </div>
