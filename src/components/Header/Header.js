0a15a1395e46f05a6a1a78875899bc955bb69eb5
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-05-27 19:28:31 -0500

    header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React, { Component} from 'react';
+
+const Header = props => (
+    <div className="page-header">
+    <h1>Welcome to mix-tape <small>Travel Playlist solution</small></h1>
+  </div>
+)
+
+export default Header;
