a3c130d9851fb3edb6ff98099b4c15671a6dd3cc
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-05-29 19:14:47 -0500

    adding more layout elements to page

--- src/App.js
+++ src/App.js
@@ -2,19 +2,64 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import Navbar from './components/Navbar'
 import Footer from './components/Footer';
+
+//  antd
+
+import {Card} from 'antd';
+
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
       <Header />
-      {/* <div className="App">
-        <p className="App-intro">
-          Components go here 
-        </p>
-      </div> */}
-      
-      <Footer />
+      <div className="main-content" style={{ padding: "5em" }}>
+          <div className="workspace">
+            <div style={{ marginTop: "33px",flexWrap:"unset"}} className="row">
+              <div className="col-md-6">
+      <Card
+    hoverable
+    style={{
+      width: 200,
+      float: "left",
+      marginBottom: 40,
+      height: 305,
+      marginRight: 30
+    }}
+    cover={
+      <img
+        style={{ height: "186px" }}
+        alt="example"
+        src={"https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"}
+      />
+    }
+  >
+  </Card>
+
+  <Card
+    hoverable
+    style={{
+      width: 200,
+      float: "left",
+      marginBottom: 40,
+      height: 305,
+      marginRight: 30
+    }}
+    cover={
+      <img
+        style={{ height: "186px" }}
+        alt="example"
+        src={"https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"}
+      />
+    }
+  >
+  </Card>
+    
+    </div>
+    </div>
+    </div>
+    </div>
+    <Footer />
     </div>
     );
   }
--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -3,8 +3,34 @@ import React, {Component} from 'react';
 import Navbar from '../../Navbar';
 import Footer from '../../Footer';
 
-class Home extends component {
+//  Ant D 
+import {Card} from 'antd';
 
+class Home extends Component {
+  render() {
+    <Card
+    hoverable
+    style={{
+      width: 200,
+      float: "left",
+      marginBottom: 40,
+      height: 305,
+      marginRight: 30
+    }}
+    cover={
+      <img
+        style={{ height: "186px" }}
+        alt="example"
+        src={"https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"}
+      />
+    }
+  >
+  </Card>
+
+  }
+  return (
+      
+  )
 }
 
 
